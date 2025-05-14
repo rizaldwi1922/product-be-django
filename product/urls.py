077bcde00fd8e8537bd6bda57d6278fa02474ddb
Rizal <rizaldwi2708@gmail.com>
2025-05-15 00:35:36 +0700

    update

--- product/urls.py
+++ product/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ProductListView, CartView
 
 urlpatterns = [
-    path('products', ProductListView.as_view(), name='produk-list'),
-    path('cart', CartView.as_view()),
+    path('products/', ProductListView.as_view(), name='produk-list'),
+    path('cart/', CartView.as_view()),
 ]
